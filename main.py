e6a5e628728f9482050afdb30328b09fe5382d7e
Alex <bktato@gmail.com>
2023-07-21 12:14:09 +0200

    test2

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from wsgiref.simple_server import make_server
 
 #env = dicc q almacena info de las peticiones del cliente (incluye peticion, headers...)
 # start_response = callback que recibe 2 argumentos (statuscode y headers que envia el server al cli)
+
+#creo constante html donde alojo web
+HTML ="""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SERVIDOR EN PYTHON</title>
+    </head>
+    <body>
+        <h1>Hola desde el server ligero python</h1>
+    </body>
+</html>
+        
+"""
+
 def application(env, start_response):
-    headers=[('Content-Type', 'text/plain')]
+    headers=[('Content-Type', 'text/html')]
     start_response('200 OK', headers)
-    
-    return ['Hola desde el server ligero python'.encode('utf-8')]
+    # devuelvo bytes con codificacion utf-8 y la web alojada en constante html
+    return [bytes(HTML,'utf-8')]
 
 # creamos el servidor
 #make_server recibe obligaotiriamente 3 argumentos
